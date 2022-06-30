208a4138347a6fad293e2d5e004924f7ac43643b
Tobae <75417176+TOBAE@users.noreply.github.com>
2022-06-30 02:36:33 +0100

    Update script.js

--- To-do_App/sign_in/script.js
+++ To-do_App/sign_in/script.js
@@ -1,35 +1,53 @@
 const email = document.getElementById("email");
 const password = document.getElementsByClassName("pass");
-const form = document.getElementsByClassName("form");
-const button = document.getElementById("btn");
-const error = document.getElementById("error");
-const error_2 = document.getElementsByClassName("password")
+const form = document.querySelector("#form");
 
-
-function Validate() {
-    if (email.value.trim() == "") {
-       error.style.display = "block" 
-    }else{
-        error.style.display = "none"
-    }
-
-    if (password.value.trim() === "") {
-      return  alert('its working')
-     }else{
-         error_2.style.visibility = "hidden"
-     }
+const isValidEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 
-var raw = "{\r\n    \r\n	\"email\": \"adegbite@gmail.com\",\r\n	\"password\": \"adeola\"\r\n    \r\n}";
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
 
-var requestOptions = {
-  method: 'POST',
-  body: raw,
-  redirect: 'follow'
-};
+if(email.value === ''){
+  email.classList.add('error');
+  alert('Email is required');
+}else if (!isValidEmail) {
+  alert('Provide a valid email address');
+}
 
-fetch("https://todo22a.herokuapp.com/api/v1/user/login", requestOptions)
-  .then(response => response.json())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+if(password.value === ''){
+  password.classList.add('error');
+  alert('Password is required');
+} 
+
+else{
+  const prePayload = new FormData(form);
+  const payload = new URLSearchParams(prePayload);
+  console.log([...payload]);
+  fetch("https://todo22a.herokuapp.com/api/v1/user/login", {
+      method: 'POST',
+      body: payload,
+      redirect: 'follow'
+  })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log('err', err));
+  setTimeout(openNew, 3000)
+  function openNew() {
+      if(email.value === '' || password.value === '' ){
+          return false
+      }else{
+        alert("Welcome!");
+        window.open("../skip/index.html", target = "_self")
+      }
+
+  }
+}
+
+
+});
+
+
