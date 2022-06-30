4851820e664e2835c5f98c81ad29b3249f261281
Tobae <75417176+TOBAE@users.noreply.github.com>
2022-06-30 02:42:53 +0100

    Update script.js

--- To-do_App/SIGN_UP/script.js
+++ To-do_App/SIGN_UP/script.js
@@ -1,29 +1,56 @@
 const email = document.getElementById("email");
+const username = document.getElementById("username");
 const password = document.getElementsByClassName("pass");
-const form = document.getElementsByClassName("form");
-const button = document.getElementById("btn");
-const error = document.getElementById("error");
-const error_2 = document.getElementsByClassName("password")
+const form = document.querySelector("#form");
 
-
-function Validate() {
-    if (email.value.trim() == "") {
-       error.style.display = "block" 
-    }else{
-        error.style.display = "none"
-    }
+const isValidEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
-var raw = 'https://todo22a.herokuapp.com/api/v1/user/register'
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
 
 
-var requestOptions = {
-  method: 'POST',
-  body: raw,
-  redirect: 'follow'
-};
+if(username.value === ''){
+  username.classList.add('error');
+  alert('Name is required');
+}
+if(email.value === ''){
+  email.classList.add('error');
+  alert('Email is required');
+}else if (!isValidEmail) {
+  alert('Provide a valid email address');
+}
+if(password.value === ''){
+  password.classList.add('error');
+  alert('Password is required');
+} 
 
-fetch("https://todo22a.herokuapp.com/api/v1/user/register", requestOptions)
-  .then(response => response.json())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+else{
+  const prePayload = new FormData(form);
+  const payload = new URLSearchParams(prePayload);
+  console.log([...payload]);
+  fetch('https://todo22a.herokuapp.com/api/v1/user/register', {
+      method: 'POST',
+      body: payload,
+      redirect: 'follow'
+  })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log('err', err));
+  setTimeout(openNew, 3000)
+  function openNew() {
+      if(username.value === '' || email.value === '' || password.value === '' ){
+          return false
+      }else{
+        alert("SUCCESSFUL, Kindly Login on Next Page!");
+        window.open("../sign_in/index.html", target = "_self")
+      }
+
+  }
+}
+
+
+});
